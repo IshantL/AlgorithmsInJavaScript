369102af4831ba6198bdba28e84eaf21742ed915
Ishant Lambhate <ishantlambhate@gmail.com>
2019-02-01 12:50:44 +0000

    Update reverseWords.js

--- reverseWords.js
+++ reverseWords.js
@@ -1,6 +1,16 @@
 //reverse words
 
 function reverseWords(string){
-
+    var revArr = [];
+    var arr = string.split(" ");
+    arr.forEach(element => {
+        var revstring = "";
+        element.split('').forEach(tmp =>{
+            revstring = tmp+revstring;
+        });
+        revArr.push(revstring);
+    });
+    return revArr.join(" ");
 }
-reverseWords("Hello my name is Ishant");
+var ans = reverseWords("Hello my name is Ishant");
+console.log(ans);
