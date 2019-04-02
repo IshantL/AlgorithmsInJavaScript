85ead078689a20ddff7d51df56eb983f2f541ffa
IshantL <ishantlambhate@gmail.com>
2019-04-02 04:49:58 +0000

    update

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,10 +1,45 @@
-function fizzBuzz(num) {
-  for (var i = 1; i <= num; i++) {
-    if (i % 15 === 0) console.log('FizzBuzz');
-    else if (i % 3 === 0) console.log('Fizz');
-    else if (i % 5 === 0) console.log('Buzz');
-    else console.log(i);
-  }
-}
- 
-fizzBuzz();
+/*
+
+It will be a function that takes in a number as a parameter which we will call numb what this function
+
+will do is it will log out to the consul every number from 1 to numb so if we pass the number 20 into
+
+our fizz buzz function we should see the numbers 1 through 20 logged out in our console like this.
+
+But that is not all that our algorithm will do.
+
+For each number if the number is divisible by three we want to log out the word fizz instead of that
+
+number if the number is divisible by five.
+
+We want to log out the word buzz instead of that number.
+
+And if a number is divisible by both 3 and 5 we want to log out the word fizz buzz instead of that number.
+
+So instead of simply logging out each number from 1 to 20 we would expect our algorithm to log out a
+
+result that looks like this
+
+in which every number that is divisible by 3 and 5 is replaced by the word fizzer does every number
+
+that is divisible by three is replaced by the word fizz and every number that is divisible by 5 is replaced
+
+by the word buzz.
+ */
+
+ fizzbuzz= () => {
+     var i =1;
+    while(i<=20){
+        if(i % 3 === 0 && i % 5 === 0)
+        console.log("fizzbuzz");
+        else if( i % 3 === 0)
+        console.log("fizz");
+        else if( i % 5 === 0)
+        console.log("buzz");
+        else
+        console.log(i);
+        i++;
+    }
+ }
+
+ fizzbuzz();
