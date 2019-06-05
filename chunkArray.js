10de18b0c56097248e21c5a28168c2f7f3a5f4ce
IshantL <ishantlambhate@gmail.com>
2019-06-05 05:30:58 +0000

    chunk sol2

--- chunkArray.js
+++ chunkArray.js
@@ -13,3 +13,17 @@ const arrayChunk = (array,size)=>{
     }
     const ans = arrayChunk([1,2,3,4],2);
     console.log(ans);
+
+    //solution 2
+
+    const chunk2 = (array,size)=>{
+        debugger;
+        var chuncked =[];
+        var i =0;
+        while(i<array.length){
+        chuncked.push(array.slice(i,i+size));
+        i = i+size;
+        }
+        return chuncked;
+        }
+        chunk2([1,2,3,4],2);
